98059b74bf66b792ba75d834996bed34bb0a8949
Art Vandelay <art@vandelayindustries.invalid>
2018-10-28 13:30:08 -0400

    Custom name and email configuration functional

--- vandelay.sh
+++ vandelay.sh
@@ -1,19 +1,48 @@
 #!/bin/bash
 
-subcommand=$1
+DEFAULT_NAME="Art Vandelay"
+DEFAULT_EMAIL="art@vandelayindustries.invalid"
 
-NAME="Art Vandelay"
-EMAIL="art@vandelayindustries.invalid"
+NAME=$DEFAULT_NAME
+EMAIL=$DEFAULT_EMAIL
 
-case $subcommand in
-  "config")
-    #modify the current vandelay configuration
+config_location=~/.vandelayconfig
+
+if [ -f $config_location ]; then
+  source $config_location
+fi
+
+case $1 in
+  config)
+    #need to shift off the "config" arg so the options are first
+    #this is required for getopts
+    shift 
+    while getopts ":n:e:" opt; do
+      case $opt in
+        n)
+          NAME=$OPTARG
+          ;;
+        e)
+          EMAIL=$OPTARG
+          ;;
+      esac
+    done
+
+    #overwrite the config
+    echo "NAME=\"$NAME\"" > $config_location
+    echo -n "EMAIL=\"$EMAIL\"" >> $config_location
+
+    #print it out
+    echo
+    cat $config_location
+    echo
     ;;
-  "reset")
+  reset)
     #resets the configuration to default
+    rm $config_location
     ;;
 
-  "help")
+  help)
     echo "vandelay:"
     echo "    Commt to git with an alter-ego."
     echo
@@ -34,14 +63,14 @@ case $subcommand in
   *) 
     #didn't match a vandelay subcommand
     #assume $1 is part of the desired git commit arguments and attempt a commit
-    AUTHOR="${NAME} <${EMAIL}>"
+    AUTHOR="$NAME <$EMAIL>"
 
     echo
-    echo -e "Commiting as \"${AUTHOR}\"."
+    echo -e "Commiting as \"$AUTHOR\"."
     echo
 
     #kind of weird.. online discussion says that $@ doesn't preserve quotes and stuff
     #However, if we don't acces or manipulate $@ before usage in git commit it works
-    git -c user.name="${NAME}" -c user.email="${EMAIL}" commit --author="${AUTHOR}" "$@"
+    git -c user.name="$NAME" -c user.email="$EMAIL" commit --author="$AUTHOR" "$@"
     ;;
 esac
